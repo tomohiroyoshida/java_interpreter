de7e11d4e1a470bffaba379629a3b57282900e7a
tomo <tomohiroyoshida0511@gmail.com>
2021-02-19 04:44:52 +0900

    ADd

---
+++ chap_2/LexerReader.java
@@ -0,0 +1,27 @@
+package chap_2;
+
+import java.io.IOException;
+import java.io.Reader;
+
+class LexerReader {
+  private Reader reader;
+  private boolean unget_p = false;
+  private int ch;
+
+  public LexerReader(Reader r) {
+    reader = r;
+  }
+
+  public int read() throws IOException {
+    if (unget_p) {
+      unget_p = false;
+    } else {
+      ch = reader.read();
+    }
+    return ch;
+  }
+
+  public void unread(int c) {
+    unget_p = true;
+  }
+}
