7aff61fd719d7f694817ca67c08969a12c591f33
tomo <tomohiroyoshida0511@gmail.com>
2021-02-19 01:44:25 +0900

    Finished chap_1

---
+++ chap_1/sample.java
@@ -0,0 +1,16 @@
+class Sample {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+  }
+}
+
+// class Add {
+// public int add(int x, int y) {
+// return x + y;
+// }
+
+// public static void main(String[] args) {
+// System.out.println(add(x));
+// }
+// }
